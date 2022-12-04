f3f7d3e3d2b189d4c20e0b831d9efc1a71a4085d
Patrick Brito <patrickbrito95@gmail.com>
2022-12-04 18:58:47 -0300

    build: deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { HomePage } from '../screens/Home'
 
-import {ResultsPage} from '../screens/Results/index'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
